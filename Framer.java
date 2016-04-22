2c3c7fa94fe3e57b62027fd75d4633fc7b300b9d
AlfredoFi <theproductserial@hotmail.com>
2016-04-22 07:21:03 -0500

    Update Framer.java

--- Framer.java
+++ Framer.java
@@ -1,7 +1,7 @@
-import java.io.IOException;
+import java.io.IOException; // se importan las excepciones para cachar cualquier evento no deseado
 import java.io.OutputStream;
 
 public interface Framer{
-  void frameMsg(byte[] message, OutputStream out) throws IOException;
-  byte[] nextMsg() throws IOException;
+  void frameMsg(byte[] message, OutputStream out) throws IOException; 
+  byte[] nextMsg() throws IOException;  //arreglo byte de mensajes a partir de IOException
 }
