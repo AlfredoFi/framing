baa4a5040dbb05228ee4c85376d2cd4695a58133
AlfredoFi <theproductserial@hotmail.com>
2016-04-22 07:26:51 -0500

    Update DelimFramer.java

--- DelimFramer.java
+++ DelimFramer.java
@@ -1,36 +1,37 @@
 import java.io.*;
 
-public class DelimFramer implements Framer{
+public class DelimFramer implements Framer{   //clase DelimFramer hereda de Framer
   private InputStream in;
-  private static final char DELIMITER = '\n';
+  private static final char DELIMITER = '\n';  //Declaración de parametros
 
   public DelimFramer(InputStream in){
-    this.in = in;
+    this.in = in;     //Declaración del constructor para DelimFramer
   }
 
-  public void frameMsg(byte[] message, OutputStream out) throws IOException{
-    for(byte b : message){
+  public void frameMsg(byte[] message, OutputStream out) throws IOException{  // Framer
+    for(byte b : message){   //Ciclo para crear la seuencia del delimitador
       if(b == DELIMITER)
         throw new IOException("Delimiter is part of message");
     } 
+    //impresión de resultados
     out.write(message);
     out.write(DELIMITER); 
-    out.flush();
+    out.flush();          
   }
 
   public byte[] nextMsg() throws IOException{
-    ByteArrayOutputStream message = new ByteArrayOutputStream();
-    int nextByte;
-    while((nextByte = this.in.read()) != DELIMITER){
+    ByteArrayOutputStream message = new ByteArrayOutputStream();  //Creación de objeto message tipo ByteArray
+    int nextByte; 
+    while((nextByte = this.in.read()) != DELIMITER){  // ciclo para revisón de cada byte
       if(nextByte == -1){
         if(message.size() == 0) {
           return null;
         }else{
-          throw new EOFException("Delimiter is not part of message");
+          throw new EOFException("Delimiter is not part of message"); // Excepción indica que no es parte del frame.
         }
       }
       message.write(nextByte);
     }
-    return message.toByteArray();
+    return message.toByteArray();  // resultado del mensaje
   }
 }
